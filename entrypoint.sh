12a58c0a9f27f390fddc26992dee4191ef08238d
miensoap <miensop51@gmail.com>
2024-10-23 21:24:01 +0900

    fix. 스크립트가 끝나면 환경 변수 유지 안됨

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -l
 
 echo "$1" | while read -r line; do
-  if [[ ! -z "$line" && "$line" != \#* ]]; then
-    export $line                       
+  if [[ ! -z "$line" && "$line" != \#* ]]; then  
     key=$(echo $line | cut -d '=' -f 1)
+    value=$(echo $line | cut -d '=' -f 2-)
+
+    echo "$key=$value" >> $GITHUB_ENV
     echo "Setting environment variable: $key"
   fi
 done
